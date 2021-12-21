7528605ee9a6544fb643481db1fe725c9d68d2c7
tenderking <gmu003@gmail.com>
2021-12-22 00:40:47 +0100

    javascript hamburger fuctionality added

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import './style.css'
+const button = document.querySelector('.icon');
+
+button.addEventListener('click', event => {
+  myFunction()
+});
 
 
+
+    function myFunction() {
+     var x = document.getElementById("myTopnav");
+        console.log('u have clicked me')
+        if (x.className === "navUl") {
+          x.className += " responsive";
+        } else {
+          x.className = "navUl";
+        }
+      }
+     
