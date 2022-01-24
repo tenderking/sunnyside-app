5726f1ba35e3d6131e12cccac6e0a4e55b72b94f
George Mushore <gmu003@gmail.com>
2022-01-24 14:25:43 +0100

    changed based on feedback

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
+import "./style.css";
+const button = document.querySelector(".icon");
 
-const button = document.querySelector('.icon');
-
-button.addEventListener('click', event => {
-  myFunction()
+button.addEventListener("click", (event) => {
+	myFunction();
 });
 
-
-
-    function myFunction() {
-     var x = document.getElementById("myTopnav");
-        console.log('u have clicked me')
-        if (x.className === "navUl") {
-          x.className += " responsive";
-        } else {
-          x.className = "navUl";
-        }
-      }
-     
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	console.log("u have clicked me");
+	if (x.className === "navUl") {
+		x.className += " responsive";
+	} else {
+		x.className = "navUl";
+	}
+}
